abff596608a2dfff799ae32636807b4850cc59a9
Renn <8340896+AkazaRenn@users.noreply.github.com>
2025-12-19 22:29:49 -0500

    Improve sending keys

--- Core/Common.cpp
+++ Core/Common.cpp
@@ -5,21 +5,34 @@ import <vector>;
 import <Windows.h>;
 
 namespace Core {
+inline void SetKey(WORD key, std::unique_ptr<INPUT[]>& inputs, size_t downIndex, size_t upIndex) {
+    DWORD flags = 0;
+    if (key == VK_RMENU || key == VK_RCONTROL || key == VK_RSHIFT) {
+        flags = KEYEVENTF_EXTENDEDKEY;
+    }
+    
+    inputs[downIndex] = INPUT {
+        .type = INPUT_KEYBOARD,
+        .ki = {
+            .wVk = key,
+            .dwFlags = flags,
+        },
+    };
+    inputs[upIndex] = INPUT {
+        .type = INPUT_KEYBOARD,
+        .ki = {
+            .wVk = key,
+            .dwFlags = flags | KEYEVENTF_KEYUP,
+        },
+    };
+}
+
 inline UINT SendKeySequence(std::vector<WORD> keys) {
     size_t numInputs = keys.size() * 2;
     auto inputs = std::make_unique<INPUT[]>(numInputs);
 
     for (size_t i = 0; i < keys.size(); i++) {
-        inputs[i * 2].type = INPUT_KEYBOARD;
-        inputs[i * 2].ki.wVk = keys[i];
-        inputs[i * 2 + 1].type = INPUT_KEYBOARD;
-        inputs[i * 2 + 1].ki.wVk = keys[i];
-        inputs[i * 2 + 1].ki.dwFlags = KEYEVENTF_KEYUP;
-
-        if (keys[i] == VK_RMENU) {
-            inputs[i].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
-            inputs[numInputs - 1 - i].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
-        }
+        SetKey(keys[i], inputs, i * 2, i * 2 + 1);
     }
 
     return SendInput(static_cast<UINT>(numInputs), inputs.get(), sizeof(INPUT));
@@ -30,16 +43,7 @@ inline UINT SendKeyCombination(std::vector<WORD> keys) {
     auto inputs = std::make_unique<INPUT[]>(numInputs);
 
     for (size_t i = 0; i < keys.size(); i++) {
-        inputs[i].type = INPUT_KEYBOARD;
-        inputs[i].ki.wVk = keys[i];
-        inputs[numInputs - 1 - i].type = INPUT_KEYBOARD;
-        inputs[numInputs - 1 - i].ki.wVk = keys[i];
-        inputs[numInputs - 1 - i].ki.dwFlags = KEYEVENTF_KEYUP;
-
-        if (keys[i] == VK_RMENU) {
-            inputs[i].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
-            inputs[numInputs - 1 - i].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
-        }
+        SetKey(keys[i], inputs, i, numInputs - i - 1);
     }
 
     return SendInput(static_cast<UINT>(numInputs), inputs.get(), sizeof(INPUT));
